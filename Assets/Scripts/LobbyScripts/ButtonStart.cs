8e8e7a43e0ba3687e36678d04008d8e60b738c54
Alisa Soroka <asoroka@student.unit.ua>
2019-06-08 21:33:06 +0300

    button start can be seen just on master client

---
+++ Assets/Scripts/LobbyScripts/ButtonStart.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStart : MonoBehaviour {
+
+	void Start ()
+    {
+		if (!PhotonNetwork.IsMasterClient)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+	
+
+}
