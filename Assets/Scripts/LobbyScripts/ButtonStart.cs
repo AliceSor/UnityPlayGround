909d2678447cde9992782b8f8503c6303c2f6a5c
Alisa Soroka <asoroka@student.unit.ua>
2019-06-20 01:46:19 +0300

    Fix little error

--- Assets/Scripts/LobbyScripts/ButtonStart.cs
+++ Assets/Scripts/LobbyScripts/ButtonStart.cs
@@ -4,9 +4,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonStart : MonoBehaviour
+public class ButtonStart : MonoBehaviourPunCallbacks
 {
     public GameObject button;
+
 	public void OnEnterRoom ()
     {
 		if (!PhotonNetwork.IsMasterClient)
@@ -15,7 +16,19 @@ public class ButtonStart : MonoBehaviour
                 button.SetActive(false);
             Debug.Log("Not master client");
         }
+        else
+        {
+            if (button != null)
+                button.SetActive(PhotonNetwork.IsMasterClient);
+        }
 	}
+
+    public override void OnJoinedRoom()
+    {
+        if (button != null)
+            button.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
 
     public void OnMasterClientChanged(object obj)
     {
